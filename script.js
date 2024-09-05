8295a8fc309c8444cfc24a98bc02a9e6193cc2fc
Obam-dev <botku109@gmail.com>
2024-09-05 19:59:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-// Fungsi untuk menampilkan notifikasi "Auto HS ON"
-function activateAutoHS() {
-    // Ambil elemen notifikasi
-    var notification = document.getElementById("notification");
-
-    // Tampilkan notifikasi
-    notification.classList.remove("hidden");
-
-    // Setelah 3 detik, sembunyikan notifikasi lagi
-    setTimeout(function() {
-        notification.classList.add("hidden");
-    }, 3000);
+// Fungsi untuk mengubah status tombol dari OFF ke ON dan sebaliknya
+function toggleOption(button) {
+    if (button.innerText === "OFF") {
+        button.innerText = "ON";
+        button.classList.add("active");
+    } else {
+        button.innerText = "OFF";
+        button.classList.remove("active");
+    }
 }
+    
